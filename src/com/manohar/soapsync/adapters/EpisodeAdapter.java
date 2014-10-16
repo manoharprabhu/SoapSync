eb829e073a22796d412d1c79836d38a3507aef13
manoharprabhu <manoharagent13@gmail.com>
2014-10-16 16:57:54 +0530

    test commit to check ssh push

--- src/com/manohar/soapsync/adapters/EpisodeAdapter.java
+++ src/com/manohar/soapsync/adapters/EpisodeAdapter.java
@@ -1,14 +1,12 @@
 package com.manohar.soapsync.adapters;
 
 import android.content.Context;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.manohar.soapsync.R;
